f7d76cadb40baa1904ba9793c74d590e94f17b9c
Anke Chinni Krishna <achinnikrishna2003@gmail.com>
2026-02-11 19:57:35 +0530

    Definition

--- app.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  let role = document.getElementById("role").value;
-
-  if (role === "Student") {
-    window.location.href = "dashboard.html";
-  } else {
-    window.location.href = "faculty.html";
-  }
-}
